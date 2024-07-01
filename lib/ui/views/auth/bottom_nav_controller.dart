9cac65912284a8eec6d27392ffc031f797b4f2a9
mahir2995 <139786025+mahir2995@users.noreply.github.com>
2024-07-02 01:16:36 +0600

    3rd checkpoint

--- lib/ui/views/auth/bottom_nav_controller.dart
+++ lib/ui/views/auth/bottom_nav_controller.dart
@@ -1,11 +1,44 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:shoesphere/const/app_colors.dart';
+import 'package:shoesphere/ui/views/nav_pages/cart.dart';
+import 'package:shoesphere/ui/views/nav_pages/favourite.dart';
+import 'package:shoesphere/ui/views/nav_pages/profile.dart';
+import '../nav_pages/home.dart';
 
 class BottomNavController extends StatelessWidget {
-  const BottomNavController({super.key});
+  BottomNavController({super.key});
+
+  RxInt _currentIndex = 0.obs;
+  final _pages = [
+    Home(),
+    Favourite(),
+    Cart(),
+    Profile(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(() => Scaffold(
+      bottomNavigationBar: SalomonBottomBar(
+        selectedItemColor: AppColors.mandarinColor,
+        currentIndex: _currentIndex.value,
+        onTap: (val) {
+          _currentIndex.value = val;
+        },
+        items: [
+          bottomBarItem(Icons.home_filled, 'Home'),
+          bottomBarItem(Icons.favorite_outline, 'Favourite'),
+          bottomBarItem(Icons.shopping_bag_outlined, 'Cart'),
+          bottomBarItem(Icons.person_outline, 'Person'),
+        ],
+      ),
+      body: _pages[_currentIndex.value],
+    ));
   }
 }
+
+SalomonBottomBarItem bottomBarItem(icon, title) =>
+    SalomonBottomBarItem(icon: Icon(icon), title: Text(title));
