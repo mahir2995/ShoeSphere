5b0cbf502bab04064c4f136e6a382130203f1383
Likhon <147406131+LikhonSa@users.noreply.github.com>
2024-07-16 15:32:32 +0600

    Final submission

--- lib/ui/views/auth/login.dart
+++ lib/ui/views/auth/login.dart
@@ -485,5 +485,173 @@ class Login extends StatelessWidget {
   {
     Get.find<AuthController>().signInWithGoogle();
   }
-
+ @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body : SafeArea(
+        child: Padding(
+          padding : const EdgeInsets.symmetric(horizontal:20),
+          child: SingleChildScrollView(
+            scrollDirection:Axis.vertical,
+            child:Container(
+              height:SizeConfig.screenHeight,
+                child: Form(
+                key: _formKey,
+                autovalidateMode:AutovalidateMode.always,
+                child: Column(
+                  children: [
+                    SizedBox(height:30),
+                    Center(
+                      child: Image.asset(
+                        'assets/icons/logo.png',
+                        width:40.w,
+                      ),
+                    ),
+                  SizedBox(height:10),
+                    Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height:20),
+                    customFormField(
+                      TextInputType.emailAddress,
+                      _emailController,
+                      context,
+                      'Email',
+                          (val) {
+                        if (val == null || val.isEmpty) {
+                          return 'Email cannot be empty';
+                        }
+                        if (!val.contains(RegExp(r'\@'))) {
+                          return 'enter a valid email address';
+                        }
+                        return null;
+                      },
+                      prefixIcon: Icons.email_outlined,
+                    ),
+                   customFormField(
+                     TextInputType.text,
+                     _passwordController,
+                     context,
+                     'Password',
+                       (val){
+                       if(val==null || val.isEmpty)
+                         {
+                           return 'Password cannot be empty';
+                         }
+                       return null;
+                       },
+                     prefixIcon: Icons.remove_red_eye_outlined,
+                    obscureText:true,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child:InkWell(
+                        onTap: ()=>Get.toNamed(forgetPass),
+                        child: Text(
+                          'Forget Password',
+                          style:TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  SizedBox(height: 30),
+                  SizedBox(
+                    width: 200,
+                    height: 45,
+                    child: customButton(
+                      'Log in',
+                          () {
+                        if (_formKey.currentState!.validate()) {
+                          Get.find<AuthController>().login(
+                            _emailController.text.trim(),
+                            _passwordController.text.trim(),
+                            context,
+                          );
+                        } else {
+                          print('failed');
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text('  OR  '),
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                SizedBox(height:20),
+                  Center(
+                    child:InkWell(
+                      borderRadius:BorderRadius.circuler(50),
+                      onTap: _handleGoogleLogin,
+                      child: Ink(
+                      height:50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                    color:AppColors.whiteColor,
+                    shape:BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                      color: AppColors.grayColor.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius:7,
+                      offset: Offset(0,3),
+                      ),
+                    ],
+                ),
+                child:padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Image.asset('assets/icons/search.png'),
+              ),
+            ),
+          ),
+        ),
+          SizedBox(height: 30),
+            Text.rich(
+            TextSpan(
+            children: [
+            TextSpan(
+            text: 'Don\'t have an account?',
+            style: TextStyle(color: AppColors.grayColor),
+            ),
+            TextSpan(
+            recognizer: TapGestureRecognizer()
+            ..onTap = () => Get.toNamed(registration),
+            text: ' Sign Up',
+            style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+            ),
+            ),
+            ],
+            ),
+            ),
+                  ],
+                ),
+                ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
